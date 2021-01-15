6f430c3fdb9845dc9c92776cb81913ea0e6b3a08
herihermwn <herih1876@gmail.com>
2021-01-15 11:05:26 +0700

    Add: Speech to Text Logic

--- app/src/main/java/org/adarmawan117/recognition/sibi/view/SpeechToTextActivity.java
+++ app/src/main/java/org/adarmawan117/recognition/sibi/view/SpeechToTextActivity.java
@@ -1,35 +1,116 @@
 package org.adarmawan117.recognition.sibi.view;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.text.Editable;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import org.adarmawan117.recognition.sibi.R;
 
-public class SpeechToTextActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class SpeechToTextActivity extends AppCompatActivity implements View.OnClickListener {
+
     public static final Integer RecordAudioRequestCode = 1;
+    private EditText resultSpeech;
+    private ImageView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_to_speech);
 
+        init();
     }
 
     private void init() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             checkPermission();
         }
+
+        ImageButton speechButton = findViewById(R.id.speechButton);
+        resultSpeech = findViewById(R.id.resultSpeech);
+        backButton = findViewById(R.id.back_button);
+
+        speechButton.setOnClickListener(this);
+        backButton.setOnClickListener(this);
     }
 
     private void checkPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, RecordAudioRequestCode);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 101) {
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                ArrayList<String> listResult = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                String result = resultSpeech.getText().toString() + listResult.get(0);
+                resultSpeech.setText(result);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == RecordAudioRequestCode && grantResults.length > 0 ){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this,"Permintaan di izinkan",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.back_button:
+                finish();
+                break;
+
+            case R.id.speechButton :
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(
+                        RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+                );
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.forLanguageTag("id-ID"));
+
+                // Check device support speech to text or not
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, 101);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Perangkat tidak mendukung text to speech", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
         }
     }
 }
